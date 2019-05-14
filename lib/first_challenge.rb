04033357e89cb677998c583fe10e964dd0be19d0
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 17:53:40 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,9 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"].each do |key,value|
     if key == :favorite_icecream_flavors
-      value.delete_if("strawberry")
+      value.delete_if |x|
+      x = "strawberry"
+    end
       end
     end
     contacts
