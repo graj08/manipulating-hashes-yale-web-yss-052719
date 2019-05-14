0304282b5b0378fd1a551b2f64bd7fdfaf02b70e
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 17:29:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,6 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if |x|
   x = "strawberry"
-end
   #remember to return your newly altered contacts hash!
   contacts
 end
